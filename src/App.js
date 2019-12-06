18c9cbcca4f7c30236a3af3d26d7e7d18d961996
Twynzen <razzziel001@gmail.com>
2019-12-06 16:28:12 -0500

    Entendiendo más de el funcionamiento de react y lo que sucede en app.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from "react";
 
 function App() {
+  const holapues = () => {
+    return "hola mundo pues";
+  };
+
+  const numero = () => {
+    console.log(1);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1> BIenvenido a react</h1>
-      </header>
+      <h1>Probando</h1>
+      {holapues()}
+      <button onClick={numero}>CLICK</button>
     </div>
   );
 }
